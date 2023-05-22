7e3a41a128eb966b927679efe35a893f1c3c7b39
ericpkatz <ericpkatz@gmail.com>
2023-05-22 11:10:18 -0400

    message

--- src/Todos.js
+++ src/Todos.js
@@ -7,50 +7,57 @@ const Todos = ()=> {
   const { categories, todos } = useSelector(state => state);
   const { term } = useParams();
   const dispatch = useDispatch();
+  const filtered = todos.filter(todo => !term || todo.name.includes(term));
+
   return (
-    <ul>
+    <div>
       {
-        todos
-        .filter(todo => !term || todo.name.includes(term))
-        .map( todo => {
-          const category = categories.find(category => category.id === todo.categoryId);
-          return (
-            <li key={ todo.id }>
-              <Link to={`/${todo.id}`}>
-                { todo.name }
-              </Link>
-              ({ category ? category.name : 'none'})
-              <button
-                onClick= {
-                  ()=> {
-                    dispatch(destroyTodo(todo));
-                  }
-                }
-              >
-              x
-              </button>
-              <select
-                value={ todo.categoryId }
-                onChange = {
-                  ev => {
-                    const updatedTodo = {...todo, categoryId: ev.target.value * 1};
-                    dispatch(updateTodo(updatedTodo));
-                  }
-                }
-              >
-                {
-                  categories.map( category => {
-                    return (
-                      <option value={ category.id } key={ category.id }>{ category.name }</option>
-                    );
-                  })
-                }
-              </select>
-            </li>
-          );
-        })
+        filtered.length !== todos.length ? (
+          <h2>You are filtering { filtered.length } out of { todos.length }</h2>
+        ): null
       }
-    </ul>
+      <ul>
+        {
+          filtered.map( todo => {
+            const category = categories.find(category => category.id === todo.categoryId);
+            return (
+              <li key={ todo.id }>
+                <Link to={`/${todo.id}`}>
+                  { todo.name }
+                </Link>
+                ({ category ? category.name : 'none'})
+                <button
+                  onClick= {
+                    ()=> {
+                      dispatch(destroyTodo(todo));
+                    }
+                  }
+                >
+                x
+                </button>
+                <select
+                  value={ todo.categoryId }
+                  onChange = {
+                    ev => {
+                      const updatedTodo = {...todo, categoryId: ev.target.value * 1};
+                      dispatch(updateTodo(updatedTodo));
+                    }
+                  }
+                >
+                  {
+                    categories.map( category => {
+                      return (
+                        <option value={ category.id } key={ category.id }>{ category.name }</option>
+                      );
+                    })
+                  }
+                </select>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
   )
 };
 
